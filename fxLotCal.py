1a9195d19991d321b211cf1b16b6965b3e98b00b
Dominic Henry <dominichenrywork@hotmail.com>
2019-09-03 07:01:58 -0500

    Error hardcoded risk, now changed

--- fxLotCal.py
+++ fxLotCal.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     risk,account_balance,stop_loss = (int(x) for x in sys.argv[1:])
 
     # Calculating dollar value of risk
-    risk_dollar_val = account_balance * (3/100) 
+    risk_dollar_val = account_balance * (risk/100) 
 
     # Calculating lot size
-    lot_size = round((stop_loss/risk_dollar_val)/1000, 2)
+    lot_size = (risk_dollar_val/stop_loss)/10
 
     print(f"Risk(USD): {risk_dollar_val}\nLot: {lot_size}")
     
